a08a7a92efa7efb6544e2eded12325549cb006ed
rohmunen <71789493+rohmunen@users.noreply.github.com>
2021-03-11 18:28:30 +0300

    fixed parse namings

--- Parse/parse_Kib.go
+++ Parse/parse_Kib.go
@@ -2,6 +2,6 @@ package Parse
 
 import TTS "Schedule/Structure"
 
-func ParseKIB(subject, typeOfLesson, teacherName, cabinet string) []TTS.Lesson {
+func ParseKIB(subject, typeOfLesson, teacherName, cabinet, dayOfWeek, numberLesson, week string) []TTS.Lesson {
 	return []TTS.Lesson{TTS.NewLesson(), TTS.NewLesson()}
 }
