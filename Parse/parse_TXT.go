915459ea0dc9ec0a6c910ed35f9303c014c1b962
rohmunen <71789493+rohmunen@users.noreply.github.com>
2021-03-16 21:12:08 +0300

    redundant files deleted

--- Parse/parse_TXT.go
+++
@@ -1,7 +0,0 @@
-package Parse
-
-import . "Schedule/Structure"
-
-func ParseTXT(subject, typeOfLesson, teacherName, cabinet, dayOfWeek, numberLesson, week string) []Lesson {
-	return []Lesson{NewLesson(), NewLesson()}
-}
--- Parse/parse_VZO.go
+++
@@ -1,7 +0,0 @@
-package Parse
-
-import . "Schedule/Structure"
-
-func ParseVZO(subject, typeOfLesson, teacherName, cabinet, dayOfWeek, numberLesson, week string) []Lesson {
-	return []Lesson{NewLesson(), NewLesson()}
-}
