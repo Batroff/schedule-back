7dd24fae3cdd12c0b971f05d72580598818a2284
Sergey Savransky <savran46@mail.ru>
2021-03-16 21:52:54 +0300

    parse all links from html

---
+++ html/html.go
@@ -0,0 +1,45 @@
+package html
+
+import (
+	"../download"
+	"bufio"
+	"fmt"
+	"os"
+	"regexp"
+)
+
+func GetLinks(filepath string) []string {
+	const (
+		scheduleURL string = "https://www.mirea.ru/schedule/"
+	)
+
+	var (
+		links []string
+	)
+
+	err := download.GetFile(filepath, scheduleURL)
+	if err != nil {
+		panic(err)
+	}
+
+	file, err := os.Open(filepath)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	urlPath := regexp.MustCompile("https://.*xlsx")
+	hrefElement := regexp.MustCompile("<a.*class=\"uk-link-toggle\".*href=\"https://.*..xlsx\".*>")
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		if hrefElement.MatchString(line) {
+			links = append(links, urlPath.FindString(line))
+		}
+	}
+
+	fmt.Println(links)
+	return links
+}
