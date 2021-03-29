9e099ba8bc5108881a11348536f90c61b7bd4034
Vehitsel <vehitsel@gmail.com>
2021-03-30 01:25:58 +0300

    sorting different cases into subgroups

--- parse/subgroups.go
+++ parse/subgroups.go
@@ -7,137 +7,290 @@ import (
 	"strings"
 )
 
+//недели или нет предмет подгруппа
+var SimpleSubgroupRegexp1 = regexp.MustCompile(`((\d{1,2},?\w?)*( *н?\.? *-* *))([А-Яа-я]+ *-*,*\.* *)+(\(\d\s*п/г\)|\d ?гр ?$|\(?\d ?подгр\.? ?\)?$)`)
+
+//недели - гр1 недели - гр2 предмет
+var SimpleSubgroupRegexp2 = regexp.MustCompile(`((\d{1,2},? ?)н ?- ?\d ?гр,? ?)+ *([А-Яа-я]* *)*`)
+
+//гр. недели предмет
+var SimpleSubgroupRegexp3 = regexp.MustCompile(`\dгр\. ?(\d{1,2},?)+ ?н\.? *`)
+
+var CrutchRegexp1 = regexp.MustCompile(`,? *\d ?гр ?/ ?\d ?гр`)
+
+//несколько уроков в 1 дне надо раскидать по строкам и если одинаковые предметы почему они раскинуты(тип работы/преподы)
 func SubGroupParse(subject, typeOfLesson, teacherName, cabinet, dayOfWeek, numberLesson, week string) []Lesson {
-	//несколько уроков в 1 дне надо раскидать по строкам и если одинаковые предметы почему они раскинуты(тип работы/преподы)
 	var lessons []Lesson
 	if strings.Contains(subject, "\n") { // если в строчке с предметом более 1 строки
-		//fmt.Println("ПРЕДМЕТ:")
-		//fmt.Println(subject)
-		//fmt.Println("ТИП:")
-		//fmt.Println(typeOfLesson)
-		//fmt.Println("ФИО:")
-		//fmt.Println(teacherName)
-		//fmt.Println("КАБИНЕТ:")
-		//fmt.Println(cabinet)
-		if strings.Contains(cabinet, "В-78*\n") || strings.Contains(cabinet, "В-86*\n") || strings.Contains(cabinet, "МП-1*\n") {
-			strings.ReplaceAll(cabinet, "В-78*\n", "В-78* ")
-			strings.ReplaceAll(cabinet, "В-86*\n", "В-86* ")
-			strings.ReplaceAll(cabinet, "МП-1*\n", "МП-1* ")
-		}
-		subjects := strings.Split(subject, "\n")
-		typesOfLessons := strings.Split(typeOfLesson, "\n")
-		teachersNames := strings.Split(teacherName, "\n")
-		cabinets := strings.Split(cabinet, "\n")
-		//max := Max(len(subjects), len(typesOfLessons), len(teachersNames), len(cabinets)) // максимальное число строк в ячейке
-		RemoveLastEmptyElement(&typesOfLessons)
-		RemoveLastEmptyElement(&teachersNames)
-		RemoveLastEmptyElement(&cabinets)
-		if Contains(subjects, "…………………") { // =)
-			for i, s := range subjects {
-				if s == "…………………" {
-					RemoveElement(&subjects, i)
-				}
-			}
-		}
-		if Contains(subjects, "") {
-			for i, s := range subjects {
-				if s == "" {
-					RemoveElement(&subjects, i)
-				}
-			}
-
-			if Contains(typesOfLessons, "") {
-				for i, s := range typesOfLessons {
-					if s == "" {
-						RemoveElement(&typesOfLessons, i)
-					}
-				}
-			}
-			if Contains(teachersNames, "") {
-				for i, s := range teachersNames {
-					if s == "" {
-						RemoveElement(&teachersNames, i)
-					}
-				}
-			}
-			if Contains(cabinets, "") {
-				for i, s := range cabinets {
-					if s == "" {
-						RemoveElement(&cabinets, i)
-					}
-				}
-			}
-		}
-		FixSameSubjectParameters(&subjects, &typesOfLessons, &teachersNames, &cabinets)
-
-		collection := [][]string{
-			subjects, typesOfLessons, teachersNames, cabinets,
-		}
-
-		length := len(collection[0])
-
-		for i := 0; i < length; i++ {
-			if collection[0][i] == "" && collection[1][i] == "" && collection[2][i] == "" && collection[3][i] == "" {
-				RemoveElement(&collection[0], i)
-				RemoveElement(&collection[1], i)
-				RemoveElement(&collection[2], i)
-				RemoveElement(&collection[3], i)
-				length--
-				continue
-			}
-			someLesson := NewLesson()
-			someLesson.Subject = collection[0][i]
-			someLesson.TypeOfLesson = collection[1][i]
-			someLesson.TeacherName = collection[2][i]
-			someLesson.Cabinet = collection[3][i]
-			lessons = append(lessons, someLesson) // массив с уроками "предмет с/без п/г" "тип" "фио" "кабинет"
-		}
-		b := len(subjects)
-		if (b != len(typesOfLessons)) || (b != len(teachersNames)) || (b != len(cabinets)) || (len(typesOfLessons) != len(teachersNames)) || (len(typesOfLessons) != len(cabinets)) || (len(teachersNames) != len(cabinets)) {
-			fmt.Println("Предметы:")
-			for _, s := range subjects {
-				fmt.Println(s)
-			}
-			fmt.Println("Тип занятий")
-			for _, lesson := range typesOfLessons {
-				fmt.Println(lesson)
-			}
-			fmt.Println("ФИО")
-			for _, teachersName := range teachersNames {
-				fmt.Println(teachersName)
-			}
-			fmt.Println("Кабинет")
-			for _, s := range cabinets {
-				fmt.Println(s)
-
-			}
-		}
-
-	} else { // в строке нет энтеров
-		if !strings.Contains(subject, "яз") { // надо где языки то что возможно запарсить а то что нет так кинуть в урок
+		lessons = LessonToLessons(subject, typeOfLesson, teacherName, cabinet)
+		SubgroupLessonsSort(&lessons)
+		for _, lesson := range lessons {
+			//fmt.Println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
 			//fmt.Println("Предметы:")
-			//fmt.Println(subject)
-			//fmt.Println("Тип занятий")
-			//fmt.Println(typeOfLesson)
-			//fmt.Println("ФИО")
-			//fmt.Println(teacherName)
-			//fmt.Println("Кабинет")
-			//fmt.Println(cabinet)
-		} else { // надо парсить одиночные строки желательно бы начать с тех строк где несколько предметов и разелить их как-то
-
+			//fmt.Println(lesson.Subject)
+			//fmt.Println("Тип занятий:")
+			//fmt.Println(lesson.TypeOfLesson)
+			//fmt.Println("ФИО:")
+			//fmt.Println(lesson.TeacherName)
+			//fmt.Println("Кабинет:")
+			//fmt.Println(lesson.Cabinet)
+			SubgroupLessonParse(&lesson)
 		}
+	} else { // в строке нет энтеров
+		lesson := NewLesson()
+		lesson.Subject = subject
+		lesson.TypeOfLesson = typeOfLesson
+		lesson.TeacherName = teacherName
+		lesson.Cabinet = cabinet
+		SubgroupLessonParse(&lesson)
 	}
 	return lessons
 }
 
-func SortDataBySubgroup(subjects, typesOfLesson, teachersName, cabinets []string) {
+func SubgroupLessonParse(lesson *Lesson) []Lesson {
+	if strings.Contains(lesson.Subject, "яз") { // надо где языки то что возможно запарсить а то что нет так кинуть в урок
+		//fmt.Println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+		//fmt.Println("Предметы:")
+		//fmt.Println(lesson.Subject)
+		//fmt.Println("Тип занятий")
+		//fmt.Println(lesson.TypeOfLesson)
+		//fmt.Println("ФИО")
+		//fmt.Println(lesson.TeacherName)
+		//fmt.Println("Кабинет")
+		//fmt.Println(lesson.Cabinet)
+	} else { // надо парсить одиночные строки желательно бы начать с тех строк где несколько предметов и разелить их как-то
 
+	}
+	return []Lesson{NewLesson()}
 }
 
-//Удаляет последний пустой элемент
-func RemoveLastEmptyElement(array *[]string) {
-	if (*array)[len(*array)-1] == "" {
-		RemoveElement(array, len(*array)-1)
+func getIdLesson(lessons *[]Lesson) (array int) {
+	for i, lesson := range *lessons {
+		if CrutchRegexp1.MatchString(lesson.Subject) {
+			//fmt.Println(lessons)
+			return i
+		}
+	}
+	return -1
+}
+
+func SubgroupLessonsSort(lessons *[]Lesson) {
+	for getIdLesson(lessons) != -1 {
+		i2 := getIdLesson(lessons)
+		lesson1, lesson2 := FixSlash((*lessons)[i2])
+		*lessons = append(append((*lessons)[:i2], (*lessons)[i2+1:]...), lesson1, lesson2)
+	}
+	//fmt.Println(lessons)
+	for _, lesson := range *lessons {
+		if !SubgroupRegexp.MatchString(lesson.Subject) {
+			// lesson надо отправить в человеческий парс
+
+		} else { //парс с подгруппами
+			if CrutchRegexp1.MatchString(lesson.Subject) {
+				//fmt.Println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+				//fmt.Println("Предметы:")
+				//fmt.Println(lesson.Subject)
+				//fmt.Println("Тип занятий")
+				//fmt.Println(lesson.TypeOfLesson)
+				//fmt.Println("ФИО")
+				//fmt.Println(lesson.TeacherName)
+				//fmt.Println("Кабинет")
+				//fmt.Println(lesson.Cabinet)
+			}
+			if SimpleSubgroupRegexp1.MatchString(lesson.Subject) && !strings.Contains(lesson.Subject, ")/И") {
+				//fmt.Println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+				//fmt.Println("Предметы:")
+				//fmt.Println(lesson.Subject)
+				//fmt.Println("Тип занятий")
+				//fmt.Println(lesson.TypeOfLesson)
+				//fmt.Println("ФИО")
+				//fmt.Println(lesson.TeacherName)
+				//fmt.Println("Кабинет")
+				//fmt.Println(lesson.Cabinet)
+			} else if SimpleSubgroupRegexp2.MatchString(lesson.Subject) {
+				//fmt.Println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+				//fmt.Println("Предметы:")
+				//fmt.Println(lesson.Subject)
+				//fmt.Println("Тип занятий")
+				//fmt.Println(lesson.TypeOfLesson)
+				//fmt.Println("ФИО")
+				//fmt.Println(lesson.TeacherName)
+				//fmt.Println("Кабинет")
+				//fmt.Println(lesson.Cabinet)
+			} else if SimpleSubgroupRegexp3.MatchString(lesson.Subject) {
+				fmt.Println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+				fmt.Println("Предметы:")
+				fmt.Println(lesson.Subject)
+				fmt.Println("Тип занятий")
+				fmt.Println(lesson.TypeOfLesson)
+				fmt.Println("ФИО")
+				fmt.Println(lesson.TeacherName)
+				fmt.Println("Кабинет")
+				fmt.Println(lesson.Cabinet)
+			} else {
+				//как есть так и есть нормально не запарсить и возможно и не стоит парсить
+			}
+		}
+	}
+}
+
+//такой убогий метод для слэшей
+func FixSlash(lesson Lesson) (lesson1, lesson2 Lesson) {
+	lesson1 = NewLesson()
+	lesson2 = NewLesson()
+	if strings.Contains(lesson.Subject, "/") {
+		//fmt.Println("~~~~~~~~~~~~~ИЗ МЕТОДА КОСТЫЛЯ~~~~~~~~~~~~~~~~~~~~~~~~~~")
+		//fmt.Println("Предметы:")
+		//fmt.Println(lesson.Subject)
+		//fmt.Println("Тип занятий")
+		//fmt.Println(lesson.TypeOfLesson)
+		//fmt.Println("ФИО")
+		//fmt.Println(lesson.TeacherName)
+		//fmt.Println("Кабинет")
+		//fmt.Println(lesson.Cabinet)
+		subgroupStr := CrutchRegexp1.FindString(lesson.Subject)
+		str := strings.ReplaceAll(lesson.Subject, subgroupStr, "")
+		array := strings.Split(subgroupStr, "/")
+		lesson1.Subject = str + array[0]
+		lesson2.Subject = str + array[1]
+		arrayTypes := strings.Split(lesson.TypeOfLesson, "/")
+		arrayTeachers := strings.Split(lesson.TeacherName, "/")
+		arrayCabinets := strings.Split(lesson.Cabinet, "/")
+		if len(arrayTypes) != 1 {
+			lesson1.TypeOfLesson = arrayTypes[0]
+			lesson2.TypeOfLesson = arrayTypes[1]
+		} else {
+			lesson1.TypeOfLesson = arrayTypes[0]
+			lesson2.TypeOfLesson = arrayTypes[0]
+		}
+		if len(arrayTeachers) != 1 {
+			lesson1.TeacherName = arrayTeachers[0]
+			lesson2.TeacherName = arrayTeachers[1]
+		} else {
+			lesson1.TeacherName = arrayTeachers[0]
+			lesson2.TeacherName = arrayTeachers[0]
+		}
+		if len(arrayCabinets) != 1 {
+			lesson1.Cabinet = arrayCabinets[0]
+			lesson2.Cabinet = arrayCabinets[1]
+		} else {
+			lesson1.Cabinet = arrayCabinets[0]
+			lesson2.Cabinet = arrayCabinets[0]
+		}
+		//fmt.Println("~~~~~~~~~~~~~РЕЗУЛЬАТАТ МЕТОДА КОСТЫЛЯ 1~~~~~~~~~~~~~~~~~~~~~~~~~~")
+		//fmt.Println("Предметы:")
+		//fmt.Println(lesson1.Subject)
+		//fmt.Println("Тип занятий")
+		//fmt.Println(lesson1.TypeOfLesson)
+		//fmt.Println("ФИО")
+		//fmt.Println(lesson1.TeacherName)
+		//fmt.Println("Кабинет")
+		//fmt.Println(lesson1.Cabinet)
+		//fmt.Println("~~~~~~~~~~~~~РЕЗУЛЬТАТ МЕТОДА КОСТЫЛЯ 2~~~~~~~~~~~~~~~~~~~~~~~~~~")
+		//fmt.Println("Предметы:")
+		//fmt.Println(lesson2.Subject)
+		//fmt.Println("Тип занятий")
+		//fmt.Println(lesson2.TypeOfLesson)
+		//fmt.Println("ФИО")
+		//fmt.Println(lesson2.TeacherName)
+		//fmt.Println("Кабинет")
+		//fmt.Println(lesson2.Cabinet)
+	}
+	return lesson1, lesson2
+}
+
+func LessonToLessons(subject, typeOfLesson, teacherName, cabinet string) []Lesson {
+	var lessons []Lesson
+	if strings.Contains(cabinet, "В-78*\n") || strings.Contains(cabinet, "В-86*\n") || strings.Contains(cabinet, "МП-1*\n") {
+		strings.ReplaceAll(cabinet, "В-78*\n", "В-78* ")
+		strings.ReplaceAll(cabinet, "В-86*\n", "В-86* ")
+		strings.ReplaceAll(cabinet, "МП-1*\n", "МП-1* ")
+	}
+	subjects := strings.Split(subject, "\n")
+	typesOfLessons := strings.Split(typeOfLesson, "\n")
+	teachersNames := strings.Split(teacherName, "\n")
+	cabinets := strings.Split(cabinet, "\n")
+	RemoveLastEmptyElement(&typesOfLessons)
+	RemoveLastEmptyElement(&teachersNames)
+	RemoveLastEmptyElement(&cabinets)
+	if Contains(subjects, "…………………") { // =)
+		for i, s := range subjects {
+			if s == "…………………" {
+				RemoveElement(&subjects, i)
+			}
+		}
+	}
+	if Contains(subjects, "") {
+		for i, s := range subjects {
+			if s == "" {
+				RemoveElement(&subjects, i)
+			}
+		}
+
+		if Contains(typesOfLessons, "") {
+			for i, s := range typesOfLessons {
+				if s == "" {
+					RemoveElement(&typesOfLessons, i)
+				}
+			}
+		}
+		if Contains(teachersNames, "") {
+			for i, s := range teachersNames {
+				if s == "" {
+					RemoveElement(&teachersNames, i)
+				}
+			}
+		}
+		if Contains(cabinets, "") {
+			for i, s := range cabinets {
+				if s == "" {
+					RemoveElement(&cabinets, i)
+				}
+			}
+		}
+	}
+	FixSameSubjectParameters(&subjects, &typesOfLessons, &teachersNames, &cabinets)
+
+	parameterConversion(&subjects, &typesOfLessons)
+	parameterConversion(&subjects, &teachersNames)
+	parameterConversion(&subjects, &cabinets)
+
+	collection := [][]string{
+		subjects, typesOfLessons, teachersNames, cabinets,
+	}
+
+	length := len(collection[0])
+
+	for i := 0; i < length; i++ {
+		if collection[0][i] == "" && collection[1][i] == "" && collection[2][i] == "" && collection[3][i] == "" {
+			RemoveElement(&collection[0], i)
+			RemoveElement(&collection[1], i)
+			RemoveElement(&collection[2], i)
+			RemoveElement(&collection[3], i)
+			length--
+			continue
+		}
+		someLesson := NewLesson()
+		someLesson.Subject = collection[0][i]
+		someLesson.TypeOfLesson = collection[1][i]
+		someLesson.TeacherName = collection[2][i]
+		someLesson.Cabinet = collection[3][i]
+		lessons = append(lessons, someLesson) // массив с уроками "предмет с/без п/г" "тип" "фио" "кабинет"
+	}
+	return lessons
+}
+
+//Функция для случаев где предметов меньше чем других параметров и дублирует все лишние параметры в одной строке разделяя их вопросом
+func parameterConversion(subjects, array *[]string) {
+	if len(*subjects) < len(*array) {
+		var sum string
+		for _, s := range *array {
+			sum = sum + s + " ? "
+		}
+		*array = make([]string, len(*subjects))
+		for i := range *array {
+			(*array)[i] = sum
+		}
 	}
 }
 
@@ -149,6 +302,13 @@ func Max(number ...int) int {
 		}
 	}
 	return max
+}
+
+//Удаляет последний пустой элемент
+func RemoveLastEmptyElement(array *[]string) {
+	if (*array)[len(*array)-1] == "" {
+		RemoveElement(array, len(*array)-1)
+	}
 }
 
 //Удаляет элемент из среза строк по индексу
