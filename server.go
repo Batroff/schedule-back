2ab5cadf1549b244cb8b47c10f67036816090dff
Sergey Savransky <savran46@mail.ru>
2021-04-12 22:16:14 +0300

    rework response structure

--- server.go
+++ server.go
@@ -4,33 +4,22 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"schedule/db"
 )
-
-type HelloResponse struct {
-	Message string `json:"message"`
-}
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	query := r.URL.Query()
 	group := query.Get("group")
 
-	response := HelloResponse{
-		Message: fmt.Sprintf("Group: %s", group),
-	}
+	dbGroup := db.FindGroup("test_database", "test_collection", group)
+
+	response := dbGroup
 
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusCreated)
 
-	// 1st way
 	err := json.NewEncoder(w).Encode(response)
 	if err != nil {
 		fmt.Println(err)
 	}
-
-	// 2nd way
-	//jData, err := json.Marshal(response)
-	//if err != nil {
-	//	fmt.Println(err)
-	//}
-	//w.Write(jData)
 }
