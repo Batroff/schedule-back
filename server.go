da3131c08f123350652ea4f25cd0e0f5a53510eb
Sergey Savransky <savran46@mail.ru>
2021-04-13 02:00:08 +0300

    improve server handler: add Response struct, add return status codes

--- server.go
+++ server.go
@@ -4,19 +4,37 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"regexp"
 	"schedule/db"
+	"schedule/structure"
 )
+
+// TODO: Rework struct to interface{}
+type Response struct {
+	ErrorMsg string `json:",omitempty"`
+	Group    structure.Group
+}
+
+var groupRegexp = regexp.MustCompile(`[А-Я]{4}[-]\d{2}[-]\d{2}`)
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	query := r.URL.Query()
 	group := query.Get("group")
 
-	dbGroup := db.FindGroup("test_database", "test_collection", group)
+	var response *Response
 
-	response := dbGroup
+	if groupRegexp.MatchString(group) {
+		dbGroup := db.FindGroup("test_database", "test_collection", group)
+		response = &Response{
+			ErrorMsg: "",
+			Group:    dbGroup,
+		}
 
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusCreated)
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+	} else {
+		w.WriteHeader(http.StatusBadRequest)
+	}
 
 	err := json.NewEncoder(w).Encode(response)
 	if err != nil {
