c063367df78ab7124cd2c806cbe7f44dd6cb65f3
Vehitsel <vehitsel@gmail.com>
2021-04-17 18:50:40 +0300

    combining lessons with the same fields

--- structure/schedule_struct.go
+++ structure/schedule_struct.go
@@ -73,6 +73,29 @@ func (g Group) Clear() {
 			g.Days[s][i].SubGroup = 0
 		}
 	}
+	//объединение уроков на нечётной и чёткной недели в 1 урок
+	for s := range g.Days {
+		for i := 0; i < len(g.Days[s])-1; i++ {
+			for j := i + 1; j < len(g.Days[s]); j++ {
+				if Combined(g.Days[s][i], g.Days[s][j]) {
+					for i2 := range g.Days[s][i].OccurrenceLesson {
+						if g.Days[s][i].OccurrenceLesson[i2] || g.Days[s][j].OccurrenceLesson[i2] {
+							g.Days[s][i].OccurrenceLesson[i2] = true
+						}
+					}
+					g.Days[s][j].SubGroup = -1
+				}
+			}
+		}
+		for i := 0; i < len(g.Days[s]); i++ {
+			if g.Days[s][i].SubGroup == -1 {
+				temp := g.Days[s]
+				RemoveElementLesson(&temp, i)
+				g.Days[s] = temp
+				i--
+			}
+		}
+	}
 }
 
 func RemoveElementLesson(a *[]Lesson, i int) {
@@ -80,4 +103,19 @@ func RemoveElementLesson(a *[]Lesson, i int) {
 	//(*a)[i] = (*a)[len(*a)-1]
 	//(*a)[len(*a)-1] = Lesson{}
 	//*a = (*a)[:len(*a)-1]
+}
+
+func Combined(lesson1, lesson2 Lesson) bool {
+	if lesson1.Subject == lesson2.Subject {
+		if lesson1.TeacherName == lesson2.TeacherName {
+			if lesson1.TypeOfLesson == lesson2.TypeOfLesson {
+				if lesson1.Cabinet == lesson2.Cabinet {
+					if lesson1.NumberLesson == lesson2.NumberLesson {
+						return true
+					}
+				}
+			}
+		}
+	}
+	return false
 }
