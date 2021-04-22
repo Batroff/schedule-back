3b66bf6b57ca0e558960338ec972e42b096f61ac
Vehitsel <vehitsel@gmail.com>
2021-04-23 00:57:49 +0300

    inserting a list of groups into database added handlefunc for grouplist

--- main.go
+++ main.go
@@ -15,9 +15,14 @@ func main() {
 	if err != nil {
 		log.Panicf("%v", err)
 	}
+	err = db.InsertGroupList("test_database", "group_list")
+	if err != nil {
+		log.Panicf("%v", err)
+	}
 	//os.Getenv()
-	mux := http.NewServeMux()
-	mux.HandleFunc("/api/group", find)
+	//mux := http.NewServeMux()
+	http.HandleFunc("/api/groupList", groupListHandler)
+	http.HandleFunc("/api/group/", find)
 	log.Println("Запуск сервера на 127.0.0.1:8080...")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
