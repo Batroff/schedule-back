c75c6f585bff651cf0c20845948111b7b091be7c
Sergey Savransky <savran46@mail.ru>
2021-04-22 17:21:47 +0300

    base app pattern

--- main.go
+++ main.go
@@ -1,16 +1,23 @@
 package main
 
+import (
+	"log"
+	"net/http"
+	"schedule/db"
+	"schedule/parse"
+)
+
 func main() {
-	//groups := parse.Parse()
-	//log.Printf(string(len(groups)))
-	////for _, mini := range groupsMini {
-	////	fmt.Println(mini)
-	////}
-	//err := db.InsertMany("test_database", "test_collection", &groups)
-	//
-	//if err != nil {
-	//	log.Panicf("%v", err)
-	//}
-	//http.HandleFunc("/", handler)
-	//log.Fatal(http.ListenAndServe(":8080", nil))
+	groups := parse.Parse()
+
+	err := db.InsertMany("test_database", "test_collection", &groups)
+
+	if err != nil {
+		log.Panicf("%v", err)
+	}
+	//os.Getenv()
+	mux := http.NewServeMux()
+	mux.HandleFunc("/api/group", find)
+	log.Println("Запуск сервера на 127.0.0.1:8080...")
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
