6491c3ada80d1c1a97f6ee99b2ae26b9a55cec71
Vehitsel <vehitsel@gmail.com>
2021-04-23 00:52:21 +0300

    added response structure for group list added groupListHandler function added statuses for requests

--- server.go
+++ server.go
@@ -9,10 +9,14 @@ import (
 	"schedule/structure"
 )
 
-// TODO: Rework struct to interface{}
-type Response struct {
-	ErrorMsg string `json:",omitempty"`
-	Group    structure.Group
+type ResponseGroup struct {
+	ErrorMsg string          `json:"ErrorMsg,omitempty"`
+	Group    structure.Group `json:"Group,omitempty"`
+}
+
+type ResponseGroupList struct {
+	ErrorMsg  string              `json:"ErrorMsg,omitempty"`
+	GroupList structure.GroupList `json:"GroupList"`
 }
 
 var groupRegexp = regexp.MustCompile(`[А-Я]{4}[-]\d{2}[-]\d{2}`)
@@ -20,49 +24,63 @@ var groupRegexp = regexp.MustCompile(`[А-Я]{4}[-]\d{2}[-]\d{2}`)
 func find(w http.ResponseWriter, r *http.Request) {
 	query := r.URL.Query()
 	group := query.Get("group")
+	subgroup := query.Get("subgroup")
 
-	var response *Response
+	var response *ResponseGroup
 
 	if groupRegexp.MatchString(group) {
-		dbGroup := db.FindGroup("test_database", "test_collection", group)
-		response = &Response{
-			ErrorMsg: "",
-			Group:    dbGroup,
+		dbGroup, err := db.FindGroup("test_database", "test_collection", group, subgroup)
+		if err != nil {
+			if err.Error() == "mongo: no documents in result" {
+				w.WriteHeader(http.StatusNotFound)
+			} else {
+				w.WriteHeader(http.StatusInternalServerError)
+			}
+			response = &ResponseGroup{
+				ErrorMsg: err.Error(),
+				Group:    dbGroup,
+			}
+		} else {
+			w.WriteHeader(http.StatusOK)
+			response = &ResponseGroup{
+				ErrorMsg: "",
+				Group:    dbGroup,
+			}
 		}
 
-		w.Header().Set("Content-Type", "application/json")
-		w.WriteHeader(http.StatusOK)
 	} else {
 		w.WriteHeader(http.StatusBadRequest)
+		response = &ResponseGroup{
+			ErrorMsg: "Bad request",
+		}
 	}
-
+	w.Header().Set("Content-Type", "application/json")
 	err := json.NewEncoder(w).Encode(response)
 	if err != nil {
 		fmt.Println(err)
 	}
 }
 
-//func handler(w http.ResponseWriter, r *http.Request) {
-//	query := r.URL.Query()
-//	group := query.Get("group")
-//
-//	var response *Response
-//
-//	if groupRegexp.MatchString(group) {
-//		dbGroup := db.FindGroup("test_database", "test_collection", group)
-//		response = &Response{
-//			ErrorMsg: "",
-//			Group:    dbGroup,
-//		}
-//
-//		w.Header().Set("Content-Type", "application/json")
-//		w.WriteHeader(http.StatusOK)
-//	} else {
-//		w.WriteHeader(http.StatusBadRequest)
-//	}
-//
-//	err := json.NewEncoder(w).Encode(response)
-//	if err != nil {
-//		fmt.Println(err)
-//	}
-//}
+func groupListHandler(w http.ResponseWriter, r *http.Request) {
+	var response *ResponseGroupList
+
+	groupList, err := db.GetGroupList("test_database", "group_list")
+	if err == nil {
+		response = &ResponseGroupList{
+			GroupList: groupList,
+			ErrorMsg:  "",
+		}
+		w.WriteHeader(http.StatusOK)
+	} else {
+		fmt.Println(err)
+		response = &ResponseGroupList{
+			GroupList: structure.GroupList{},
+			ErrorMsg:  "GroupList is empty",
+		}
+		w.WriteHeader(http.StatusInternalServerError)
+	}
+	err = json.NewEncoder(w).Encode(response)
+	if err != nil {
+		fmt.Println(err)
+	}
+}
