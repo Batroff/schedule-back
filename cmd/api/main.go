651af366148645f59f6a6e48f5e326f792c7389b
Sergei Savransky <savran46@mail.ru>
2021-05-14 17:12:22 +0300

    add --debug console arg and debug entry point

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -7,9 +7,41 @@ import (
 	"github.com/batroff/schedule-back/models/config"
 	"github.com/batroff/schedule-back/server"
 	"log"
+	"os"
 )
 
 func main() {
+	if os.Args[1] == "--debug" {
+		debugMain()
+	} else {
+		prodMain()
+	}
+}
+
+func debugMain() {
+	//err := app.GetFile("/tmp/hash.xlsx", "https://webservices.mirea.ru/upload/iblock/288/ФТИ_1к_20-21_весна.xlsx")
+	//if err != nil {
+	//	log.Panic(err)
+	//}
+
+	//f, err := os.Open("/tmp/hash.xlsx")
+	//if err != nil {
+	//	log.Panic(err)
+	//}
+	//defer f.Close()
+
+	//b, _ := os.ReadFile("/tmp/hash.xlsx")
+	//hash = sha1.New()
+	//hash.Write
+
+	//hash := sha256.New()
+	//if _, err := io.Copy(hash, f); err != nil {
+	//	log.Fatal(err)
+	//}
+	//fmt.Printf("%x", hash.Sum(nil))
+}
+
+func prodMain() {
 	cfg := initConfig()
 
 	groups := excel.Parse()
