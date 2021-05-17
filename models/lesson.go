cf3e95921507b59e7040b631eff551f5fda41a3c
Sergey Savransky <savran46@mail.ru>
2021-05-17 21:45:30 +0300

    fix empty subgroups in response

--- models/lesson.go
+++ models/lesson.go
@@ -1,14 +1,13 @@
 package models
 
-// Lesson TODO: add annotations
 type Lesson struct {
-	Subject          string `json:"subject" bson:"subject"`                   //название предмета
-	TypeOfLesson     string `json:"typeOfLesson" bson:"typeOfLesson"`         //тип занятия
-	TeacherName      string `json:"teacherName" bson:"teacherName"`           //фио преподавателя
-	Cabinet          string `json:"cabinet" bson:"cabinet"`                   //кабинет
-	NumberLesson     int    `json:"numberLesson" bson:"numberLesson"`         //номер пары
-	DayOfWeek        string `json:"dayOfWeek" bson:"dayOfWeek,omitempty"`     //день недели
-	OccurrenceLesson []bool `json:"occurrenceLesson" bson:"occurrenceLesson"` //номера недель в которых присутствует эта пара
-	Exists           bool   `json:"exists,omitempty" bson:"exists,omitempty"` //для пустых пар??
-	SubGroup         int    `json:"subGroup" bson:"subGroup,omitempty"`       // номер подгруппы
+	Subject          string `json:"subject" bson:"subject"`                         //название предмета
+	TypeOfLesson     string `json:"typeOfLesson" bson:"typeOfLesson"`               //тип занятия
+	TeacherName      string `json:"teacherName" bson:"teacherName"`                 //фио преподавателя
+	Cabinet          string `json:"cabinet" bson:"cabinet"`                         //кабинет
+	NumberLesson     int    `json:"numberLesson" bson:"numberLesson"`               //номер пары
+	DayOfWeek        string `json:"dayOfWeek,omitempty" bson:"dayOfWeek,omitempty"` //день недели
+	OccurrenceLesson []bool `json:"occurrenceLesson" bson:"occurrenceLesson"`       //номера недель в которых присутствует эта пара
+	Exists           bool   `json:"exists,omitempty" bson:"exists,omitempty"`       //для пустых пар??
+	SubGroup         int    `json:"subGroup,omitempty" bson:"subGroup,omitempty"`   // номер подгруппы
 }
