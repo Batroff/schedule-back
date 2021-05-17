b885fde944c25d8b6492cab7f6a6d76d6561ae8d
Sergey Savransky <savran46@mail.ru>
2021-05-17 21:45:55 +0300

    rename responses, add hash response struct

--- models/responses.go
+++ models/responses.go
@@ -1,11 +1,16 @@
 package models
 
 type ResponseGroupList struct {
-	ErrorMsg  string     `json:"ErrorMsg,omitempty"`
-	GroupList *GroupList `json:"GroupList"`
+	ErrorMsg  string          `json:"errorMsg,omitempty"`
+	GroupList map[string]bool `json:"groupList"`
 }
 
 type ResponseGroup struct {
-	ErrorMsg string `json:"ErrorMsg,omitempty"`
-	Group    *Group `json:"Group,omitempty"`
+	ErrorMsg string `json:"errorMsg,omitempty"`
+	Group    *Group `json:"group"`
+}
+
+type ResponseHash struct {
+	ErrorMsg string   `json:"errorMsg,omitempty"`
+	Hash     []string `json:"hash"`
 }
