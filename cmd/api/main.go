759d199e5d6dc8a8a93b3c71cdfa62e03d2865c3
Sergey Savransky <savran46@mail.ru>
2021-05-17 21:42:35 +0300

    add excel hash, code comments

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"github.com/batroff/schedule-back/app"
 	"github.com/batroff/schedule-back/app/excel"
 	"github.com/batroff/schedule-back/app/hash"
@@ -25,57 +24,59 @@ func main() {
 }
 
 func debugMain() {
-	links, htmlParseErr := html.GetExcelLinks()
-	if htmlParseErr != nil {
-		log.Panicf("Error occured while html parsing. %v", htmlParseErr)
-	}
-
-	excelFiles, downloadErr := app.GetScheduleXlsx("/tmp", links[0])
-	if downloadErr != nil {
-		log.Panicf("Excel files download error. %v", downloadErr)
-	}
-
-	_, parseErr := excel.ParseMultiple(excelFiles)
-	if parseErr != nil {
-		log.Panicf("%v", parseErr)
-	}
-
-	hashes, hashErr := hash.ExcelManyTransform(excelFiles)
-	if hashErr != nil {
-		log.Panicf("%v", hashErr)
-	}
-	fmt.Println(hashes)
+	server.Start()
 }
 
 func prodMain() {
 	cfg := initConfig()
 
+	// Download html mirea.ru/schedule/
+	// Parse excel links from html file
 	links, htmlParseErr := html.GetExcelLinks()
 	if htmlParseErr != nil {
 		log.Panicf("Error occured while html parsing. %v", htmlParseErr)
 	}
 
+	// Download all excel files to ./tmp/schedule_#.xlsx
 	excelFiles, downloadErr := app.GetScheduleXlsx("/tmp", links[0])
 	if downloadErr != nil {
 		log.Panicf("Excel files download error. %v", downloadErr)
 	}
 
+	// Parse all excel files
 	groups, parseErr := excel.ParseMultiple(excelFiles)
 	if parseErr != nil {
 		log.Panicf("%v", parseErr)
 	}
 
+	// Transform all excel files to hash array
+	hashes, hashErr := hash.ExcelManyTransform(excelFiles)
+	if hashErr != nil {
+		log.Panicf("%v", hashErr)
+	}
+
+	// Set database config
 	dbConfig := database.SetConfig(cfg.Mongo.Host)
+
+	// Add groups to database
 	groupsQuery := database.SetQuery("schedule", "groups")
 	err := database.InsertGroups(dbConfig, groupsQuery, groups)
 	if err != nil {
 		log.Panicf("%v", err)
 	}
 
+	// Add list of all groups to database
 	groupListQuery := database.SetQuery("schedule", "group_list")
 	err = database.InsertGroupList(dbConfig, groupListQuery)
 	if err != nil {
 		log.Panicf("%v", err)
+	}
+
+	// Add hash of excel files to database
+	hashQuery := database.SetQuery("schedule", "hash")
+	hashErr = database.InsertHash(dbConfig, hashQuery, hashes)
+	if hashErr != nil {
+		log.Panicf("%v", hashErr)
 	}
 
 	server.Start()
